5f40e0dcd25c56d82de6c8fb60224334557153ff
emmanuelperotto <emmanuelperotto@gmail.com>
2018-04-17 12:12:19 -0300

    refatorar strings para padrão eslint

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import './index.css';
-import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
-import rootReducer from './app/store/reducers';
+import "./index.css";
+import App from "./app/App";
+import registerServiceWorker from "./registerServiceWorker";
+import rootReducer from "./app/store/reducers";
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
-  , document.getElementById('root'));
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
